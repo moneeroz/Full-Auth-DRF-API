a478f513bd15caef3e9433812939c3a6adba17f6
Moneer <moneer.alromimah@gmail.com>
2023-08-01 21:55:18 -0600

    add social auth -> google & facebook settings

--- server/settings.py
+++ server/settings.py
@@ -134,6 +134,14 @@ STATIC_ROOT = BASE_DIR / "static"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+# Django social auth
+
+AUTHENTICATION_BACKENDS = [
+    "social_core.backends.google.googleOAuth2",
+    "social_core.backends.facebook.facebookOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 # Rest framework permission classes
 
 REST_FRAMEWORK = {
@@ -152,6 +160,7 @@ DJOSER = {
     "USER_CREATE_PASSWORD_RETYPE": True,
     "PASSWORD_RESET_CONFIRM_RETYPE": True,
     "TOKEN_MODEL": None,
+    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": getenv("REDIRECT_URLS").split(","),
     "SERIALIZERS": {},
 }
 
@@ -162,6 +171,20 @@ AUTH_COOKIE_SECURE = getenv("AUTH_COOKIE_SECURE", "True") == "True"
 AUTH_COOKIE_HTTP_ONLY = True
 AUTH_COOKIE_PATH = "/"
 AUTH_COOKIE_SAMESITE = None
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = getenv("GOOGLE_AUTH_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = getenv("GOOGLE_AUTH_SECRET")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "openid",
+]
+SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ["first_name", "last_name"]
+
+SOCIAL_AUTH_FACEBOOK_KEY = getenv("FACEBOOK_AUTH_KEY")
+SOCIAL_AUTH_FACEBOOK_SECRET = getenv("FACEBOOK_AUTH_SECRET")
+SOCIAL_AUTH_FACEBOOK_SCOPE = ["email"]
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {"fields": "email, first_name, last_name"}
 
 CORS_ALLOWED_ORIGINS = getenv(
     "CORS_ALLOWED_ORIGINS", "http://localhost:3080,http://127.0.0.1:3000"
